3bb4204db8dfee848628b6b08f80294c3ffb707b
sSimoGameDev <ssimogamedev@gmail.com>
2022-07-03 22:50:42 +0200

    updated webpage

---
+++ assets/js/mailauth.js
@@ -0,0 +1,12 @@
+// auth call
+const CLIENT_ID = "670645219148-5f7edl6705d4lmaro7h01oucuh24rsv2.apps.googleusercontent.com";
+const REDIRECT_URI = "https://wincisky.github.io/trusty/test.html";
+
+const uriEnc = encodeURIComponent(REDIRECT_URI);
+const scopeEnc = encodeURIComponent("https://mail.google.com/ https://www.googleapis.com/auth/userinfo.email");
+
+const response = `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${uriEnc}&prompt=consent&response_type=code&client_id=${CLIENT_ID}&scope=${scopeEnc}&access_type=offline`;
+
+function startGoogleAuth(){
+    window.location.href = response;
+}
