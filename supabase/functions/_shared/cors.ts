f266160ea73b2b91f815fdb8dfc2f1377472355b
sSimoGameDev <ssimogamedev@gmail.com>
2022-07-17 00:04:23 +0200

    url fix

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,6 +1,6 @@
 export const corsHeaders = {
   "Content-Type": "application/json",
-  "Access-Control-Allow-Origin": "https://opentrust.it/",
+  "Access-Control-Allow-Origin": "https://opentrust.it",
   "Access-Control-Allow-Methods": "POST, OPTIONS",
   "Access-Control-Expose-Headers": "Content-Length, X-JSON",
   "Access-Control-Allow-Headers": "apikey, X-Client-Info, Content-Type, Authorization, Accept, Accept-Language, X-Authorization",
