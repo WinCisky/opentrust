4875b012a240a564bd6fd98dd8705ce336b7d5bc
Simone <xsimone97@hotmail.it>
2022-07-17 21:36:49 +0200

    testing supabase auth

--- src/assets/ts/const.ts
+++ src/assets/ts/const.ts
@@ -4,10 +4,14 @@ export interface ServerResponse {
 }
 
 export class Const {
+  static readonly urlSupabase = 
+    "https://gjclmptpvaepykpghadl.functions.supabase.co";
   static readonly urlRegitration =
     "https://gjclmptpvaepykpghadl.functions.supabase.co/opentrust/userRegistration";
   static readonly urlLogin =
     "https://gjclmptpvaepykpghadl.functions.supabase.co/opentrust/userLogin";
   static readonly anonBearer =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqY2xtcHRwdmFlcHlrcGdoYWRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTY3OTQ4OTksImV4cCI6MTk3MjM3MDg5OX0.n-WN4gsTP7HgmXjnupOtu-j0fj2hIiACEhy3NagJZt4";
+
+  static readonly urlAuthPwd = "https://gjclmptpvaepykpghadl.functions.supabase.co/auth/v1/token?grant_type=password" 
 }
--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -1,6 +1,8 @@
 import { validateRegistrationForm } from './register'
 import { validateLoginForm } from './login'
+import { logInSubmitted } from './test'
 
 
 document.querySelector('#loginForm')?.addEventListener('submit', validateLoginForm);
 document.querySelector('#registrationForm')?.addEventListener('submit', validateRegistrationForm);
+document.querySelector('#testForm')?.addEventListener('submit', logInSubmitted);
