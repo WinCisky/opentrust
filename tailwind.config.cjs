43c67c56997c932a8ddd6d57ccc46eb68e14ffdc
sSimoGameDev <ssimogamedev@gmail.com>
2022-07-21 23:54:02 +0200

    added tailwind, home revamped

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/assets/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
