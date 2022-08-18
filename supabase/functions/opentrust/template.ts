c136a5055d56951ac66022adc39b0eee8182c880
Simone <simo@MacBook-Air-di-apple.local>
2022-08-18 23:36:52 +0200

    deno deploy cron

--- supabase/functions/opentrust/template.ts
+++ supabase/functions/opentrust/template.ts
@@ -1,36 +1,40 @@
-const ENDPOINT_URL =
-  "https://neworder.deno.dev/";
+const ENDPOINT_URL = "https://neworder.deno.dev/";
 const SHOPIFY_WEBHOOK_GUIDE =
   "https://help.shopify.com/en/manual/orders/notifications/webhooks#create-webhooks";
 
-interface translation{
-    [propName: string]: string;
+interface translation {
+  [propName: string]: string;
 }
 
-const translationsIT : translation = {
-    "Your opinion is important!" : "La tua opinione conta!",
-    "You recently completed an order on " : "Hai completato di recente un ordine su ",
-    " and we'd like to hear about your experience." : "e vorremmo sapere com'è andata.",
-    "Click on&nbsp;<a href='https://opentrust.it/review/?order=" : "Clicca su&nbsp;<a href='https://opentrust.it/review/it/?order=",
-    "'><strong>this link</strong></a>&nbsp;to write about your experience." : "'><strong>questo link</strong></a>&nbsp;per raccontare la tua esperienza.",
-    "- from Opentrust.it 🪴" : "- da Opentrust.it 🪴"
-}
+const translationsIT: translation = {
+  "Your opinion is important!": "La tua opinione conta!",
+  "You recently completed an order on ":
+    "Hai completato di recente un ordine su ",
+  " and we'd like to hear about your experience.":
+    "e vorremmo sapere com'è andata.",
+  "Click on&nbsp;<a href='https://opentrust.it/review/?order=":
+    "Clicca su&nbsp;<a href='https://opentrust.it/review/it/?order=",
+  "'><strong>this link</strong></a>&nbsp;to write about your experience.":
+    "'><strong>questo link</strong></a>&nbsp;per raccontare la tua esperienza.",
+  "- from Opentrust.it 🪴": "- da Opentrust.it 🪴",
+};
 
-const translateMe = function(locale: string, toTranslate: string): string{
-    switch (locale) {
-        case "it":
-            if(toTranslate in translationsIT)
-                return translationsIT[toTranslate];
-            break;
-    }
-    // translation failed!
-    return toTranslate;
-}
+const translateMe = function (locale: string, toTranslate: string): string {
+  switch (locale) {
+    case "it":
+      if (toTranslate in translationsIT) {
+        return translationsIT[toTranslate];
+      }
+      break;
+  }
+  // translation failed!
+  return toTranslate;
+};
 
 function verifyTemplateContent(
-    link: string
-) : string {
-    return `
+  link: string,
+): string {
+  return `
     <!doctype html>
     <html>
         <head>
@@ -85,7 +89,9 @@ function registrationTemplateContent(
                 <p>Your can now login with &nbsp;<strong>${email}</strong>&nbsp; as email and &nbsp;<strong>${password}</strong>&nbsp; as password.</p>
             </div>
             <div class="row">
-                <p>You need to setup a &nbsp;<a href="${SHOPIFY_WEBHOOK_GUIDE}"><strong>shopify webhook</strong></a>&nbsp; with the following URL: &nbsp;<strong>${ENDPOINT_URL + uuid}</strong></p>
+                <p>You need to setup a &nbsp;<a href="${SHOPIFY_WEBHOOK_GUIDE}"><strong>shopify webhook</strong></a>&nbsp; with the following URL: &nbsp;<strong>${
+    ENDPOINT_URL + uuid
+  }</strong></p>
             </div>
             <div class="row">
                 <p>Set &nbsp;<strong>Order payment</strong>&nbsp; as Event, &nbsp;<strong>JSON</strong>&nbsp; as Format and the API version with tag &nbsp;<strong>Latest</strong>.</p>
@@ -100,12 +106,12 @@ function registrationTemplateContent(
 }
 
 function reviewTemplateContent(
-    shopName: string,
-    customerName: string,
-    orderId: string,
-    locale: string
-  ): string {
-    return `
+  shopName: string,
+  customerName: string,
+  orderId: string,
+  locale: string,
+): string {
+  return `
     <!doctype html>
     <html>
         <head>
@@ -123,10 +129,23 @@ function reviewTemplateContent(
                     </h2>
                 </div>
                 <div class="row">
-                    <p>${translateMe(locale, "You recently completed an order on ") + shopName + translateMe(locale, " and we'd like to hear about your experience.")}</p>
+                    <p>${
+    translateMe(locale, "You recently completed an order on ") + shopName +
+    translateMe(locale, " and we'd like to hear about your experience.")
+  }</p>
                 </div>
                 <div class="row">
-                    <p>${translateMe(locale, "Click on&nbsp;<a href='https://opentrust.it/review/?order=") + orderId + "&name=" + encodeURIComponent(customerName) + "&shop=" + encodeURIComponent(shopName) + translateMe(locale, "'><strong>this link</strong></a>&nbsp;to write about your experience.")}</p>
+                    <p>${
+    translateMe(
+      locale,
+      "Click on&nbsp;<a href='https://opentrust.it/review/?order=",
+    ) + orderId + "&name=" + encodeURIComponent(customerName) + "&shop=" +
+    encodeURIComponent(shopName) +
+    translateMe(
+      locale,
+      "'><strong>this link</strong></a>&nbsp;to write about your experience.",
+    )
+  }</p>
                 </div>
                 <br>
                 <div>
@@ -135,6 +154,10 @@ function reviewTemplateContent(
             </div>
         </body>
     </html>`;
-  }
+}
 
-export { registrationTemplateContent, verifyTemplateContent, reviewTemplateContent };
+export {
+  registrationTemplateContent,
+  reviewTemplateContent,
+  verifyTemplateContent,
+};
