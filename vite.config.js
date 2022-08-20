8d442e65e61b511b94ad15c15f7f8bbfa89fd424
Simone <simo@MacBook-Air-di-apple.local>
2022-08-20 22:54:40 +0200

    added missing pages

--- vite.config.js
+++ vite.config.js
@@ -16,11 +16,15 @@ export default defineConfig({
             input: {
                 main: resolve(root, 'index.html'),
                 review: resolve(root, 'review/index.html'),
+                reviewit: resolve(root, 'review/it/index.html'),
+                thanks: resolve(root, 'thanks/index.html'),
+                thanksit: resolve(root, 'thanks/it/index.html'),
                 mail: resolve(root, 'mail/index.html'),
                 mailcheck: resolve(root, 'mailcheck/index.html'),
                 login: resolve(root, 'login/index.html'),
                 customer: resolve(root, 'customer/index.html'),
                 orders: resolve(root, 'orders/index.html'),
+                faq: resolve(root, 'faq/index.html'),
             }
         },
         outDir: `${_dirname}/docs`,
